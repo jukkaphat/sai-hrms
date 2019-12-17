e52cea656712fcfdf784221c3c9aaacf8fa24139
jukkaphat <jukkaphat2008@hotmail.com>
2019-12-17 08:39:52 +0700

    add original hrms

---
+++ resources/views/vendor/froiden-envato/update/changelog.blade.php
@@ -0,0 +1,17 @@
+<h4 class="box-title" id="structure">Update Log</h4>
+<pre>
+    <p>
+        <strong>HRM Updates Log</strong>
+        ├──
+        │
+        │   └── <strong>Version 3.0.6</strong>
+        │       └── Profile image upload on create fix
+        │
+        │   └── <strong>Version 3.0.3</strong>
+        │       └── Support expire on update page
+        │
+        │   └── <strong>Version 2.8.1</strong>
+        │       └── One Click Update Feature added
+        └──
+    </p>
+                                        </pre>
